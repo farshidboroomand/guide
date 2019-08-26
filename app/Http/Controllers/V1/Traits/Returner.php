7a248220a41f6d1efc7a9e73b53c7141e8c81f9f
Saeed Fadakar <saeed.fadakar@gmail.com>
2019-08-26 20:56:25 +0430

    Create a returner class to organized our output structure

---
+++ app/Http/Controllers/V1/Traits/Returner.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saeed
+ * Date: 8/5/19
+ * Time: 8:17 PM
+ */
+
+namespace App\Http\Controllers\V1\Traits;
+
+
+class Returner
+{
+    /**
+     * Generate a success response
+     *
+     * @param integer       $code       HTTP status code (2XX)
+     * @param null|string   $message    Message to showing to the client
+     * @param null|array    $data       Array of data to be return to the user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function successReturner($code, $data = null, $message = null)
+    {
+        $result = [];
+        $message AND $result['message']  =   $message;
+        $data AND $result['data'] =   $data;
+
+        $this->stateReturn($data, $code);
+    }
+
+    /**
+     * Generate a failed response
+     *
+     * @param integer       $code       HTTP status code (2XX)
+     * @param null|string   $message    Message to showing to the client
+     * @param null|array    $data       Array contain information about errors occurred
+     * @param null|integer  $errorCode  Error code for exploring detail of this error
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function failureReturner($code, $errorCode, $data = null, $message = null)
+    {
+        $result = [];
+        $message AND $result['message']  =   $message;
+        $data AND $result['data'] =   $data;
+        $errorCode AND $result['error_code'] = $errorCode;
+
+        $this->stateReturn($data, $code);
+    }
+
+    /**
+     * @param integer   $code   HTTP status code
+     * @param array     $data   general data to return
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function stateReturn($data, $code)
+    {
+        return Response()->json($data, $code);
+    }
+}
