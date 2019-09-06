56ab03c2dc72f3b7948c614b364c9798a35fd0a2
Farshid Boroomand <farshidboroomand@gmail.com>
2019-09-06 14:49:04 +0430

    jobs table created

---
+++ app/Http/Controllers/V1/Authentication/Actions/VerifyAccount.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: farshid
+ * Date: 06/09/2019
+ * Time: 14:27
+ */
+
+namespace App\Http\Controllers\V1\Authentication\Actions;
+
+
+class VerifyAccount
+{
+
+}
